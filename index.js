58be5bb3d7173b988583caf2807bb5115f6708b9
mahfuj <mahfujahmad424@gmail.com>
2023-11-01 12:54:17 +0600

    initail commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("ramadan server site is running");
+});
+
+app.listen(port, () => {
+  console.log(`ramadan server site running on port ${port}`);
+});
