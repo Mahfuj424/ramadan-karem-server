91dc4fc77abcc8026a8bc4ea8472c9025c1d1e40
mahfuj <mahfujahmad424@gmail.com>
2023-11-04 18:49:37 +0600

    find with email implement

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/favorite/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await allFavoriteCollection
+        .find({ userEmail: email })
+        .toArray()
+      res.send(result)
+    })
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
